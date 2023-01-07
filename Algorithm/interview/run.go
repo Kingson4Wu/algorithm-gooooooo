7f03f595a03496684702c328fbba24a84bd0c06f
kingson4wu <kingson4wu@gmail.com>
2023-01-07 17:49:03 +0800

    algorithm-gooooooo

---
+++ Algorithm/interview/run.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"algorithm-gooooooo/Algorithm/interview/ugly"
+)
+
+func main() {
+	//fmt.Println(ugly.IsUgly(-2147483648))
+	fmt.Println(ugly.NthUglyNumber3(1000000000, 2, 217983653, 336916467))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module algorithm-gooooooo
+
+go 1.19
