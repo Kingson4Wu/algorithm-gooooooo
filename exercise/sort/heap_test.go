cc220062589fe8a3767ae8ca2914aa3a225c6a7b
kingson4wu <kingson4wu@gmail.com>
2023-03-21 15:24:20 +0800

    heapsort还是错了三个地方

--- exercise/sort/heap_test.go
+++ exercise/sort/heap_test.go
@@ -5,21 +5,23 @@ import (
 	"testing"
 )
 
-func adjust(nums []int, root int) {
+func adjust(nums []int, root, len int) {
 
-	length := len(nums)
 	//left
 	child := root*2 + 1
 
-	for child < length {
-		if child+1 < length && nums[child+1] > nums[child] {
+	for child < len {
+		if child+1 < len && nums[child+1] > nums[child] {
 			//right
 			child++
 		}
 
-		if nums[child] < nums[root] {
-			nums[child], nums[root] = nums[root], nums[child]
+		//这步别忘了 ！！！
+		if nums[child] <= nums[root] {
+			break
 		}
+
+		nums[child], nums[root] = nums[root], nums[child]
 
 		root = child
 		//left
@@ -31,13 +33,17 @@ func heapSort(nums []int) {
 
 	//init
 	for i := len(nums) / 2; i >= 0; i-- {
-		adjust(nums, i)
+		adjust(nums, i, len(nums))
 	}
 
 	//sort
 	for i := len(nums) - 1; i > 0; i-- {
-		nums[i] = nums[0]
-		adjust(nums, 0)
+		//这里是交换！！！！
+		nums[i], nums[0] = nums[0], nums[i]
+
+		//这里的调整要指定长度，因为最后的几位已经排序了，不应该继续被堆调整影响
+		//这里是i，因为是调整交换过前面的，第一次，i刚好是len(nums) - 1， 即最后一位不调整
+		adjust(nums, 0, i)
 	}
 
 }
