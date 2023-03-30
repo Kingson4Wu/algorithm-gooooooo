ddc99df949281809f069c9275597318281cafbe2
kingson4wu <kingson4wu@gmail.com>
2023-03-30 18:16:38 +0800

    139. 单词拆分

---
+++ exercise/leetcode/hot100/139. 单词拆分.go
@@ -0,0 +1,18 @@
+package hot100
+
+/**
+知道是动态规划，也没想出来
+
+1、构造一个单词的hashmap
+2、双重循环查询是否存在
+if dp[j] && wordDictSet[s[j:i]]
+    dp[i] = true
+
+leetcode/dp/139. 单词拆分.go
+
+*/
+
+func wordBreak(s string, wordDict []string) bool {
+
+	return false
+}
--- leetcode/dp/139. 单词拆分.go
+++ leetcode/dp/139. 单词拆分.go
@@ -10,6 +10,22 @@ func wordBreak(s string, wordDict []string) bool {
 
 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
 
+示例 1：
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+示例 2：
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     注意，你可以重复使用字典中的单词。
+示例 3：
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+
 来源：力扣（LeetCode）
 链接：https://leetcode.cn/problems/word-break
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
