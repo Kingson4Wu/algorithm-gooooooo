5abd9fee2b2f153a29c75d010e6819327372d0dd
kingson4wu <kingson4wu@gmail.com>
2023-03-31 15:14:52 +0800

    448. 找到所有数组中消失的数字

--- exercise/leetcode/hot100/448. 找到所有数组中消失的数字_test.go
+++ exercise/leetcode/hot100/448. 找到所有数组中消失的数字_test.go
@@ -1,5 +1,14 @@
 package hot100
 
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+似曾相识的做出来了
+利用负数和下标的技巧，替换hashmap
+*/
 /*
 *
 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
@@ -38,4 +47,10 @@ func abs(num int) int {
 		return num
 	}
 	return -num
+}
+
+func TestFindDisappearedNumbers(t *testing.T) {
+
+	fmt.Println(findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1}))
+	fmt.Println(findDisappearedNumbers([]int{1, 1}))
 }
