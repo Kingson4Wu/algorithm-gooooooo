aba9776309fc5e7fee6e43a92357c1c966949bd6
kingson4wu <kingson4wu@gmail.com>
2023-03-31 17:31:05 +0800

    647. 回文子串

---
+++ exercise/leetcode/hot100/647. 回文子串_test.go
@@ -0,0 +1,81 @@
+package hot100
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+似曾相识， 用动态规划搞定
+*/
+
+/*
+*
+给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+
+回文字符串 是正着读和倒过来读一样的字符串。
+
+子字符串 是字符串中的由连续字符组成的一个序列。
+
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+示例 1：
+
+输入：s = "abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+示例 2：
+
+输入：s = "aaa"
+输出：6
+解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+*/
+func countSubstrings(s string) int {
+
+	if len(s) == 0 {
+		return 0
+	}
+
+	if len(s) == 1 {
+		return 1
+	}
+
+	result := 0
+
+	dp := make([][]bool, len(s))
+	for i := range s {
+		dp[i] = make([]bool, len(s))
+		dp[i][i] = true
+		result++
+	}
+
+	for length := 2; length <= len(s); length++ {
+		for i := 0; i+length-1 < len(s); i++ {
+			j := i + length - 1
+			if length == 2 {
+				if s[i] == s[j] {
+					dp[i][j] = true
+					result++
+				} else {
+					dp[i][j] = false
+				}
+				continue
+			}
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1]
+				if dp[i][j] {
+					result++
+				}
+			} else {
+				dp[i][j] = false
+			}
+		}
+	}
+
+	return result
+}
+
+func TestCountSubstrings(t *testing.T) {
+	fmt.Println(countSubstrings("abc"))
+	fmt.Println(countSubstrings("aaa"))
+}
