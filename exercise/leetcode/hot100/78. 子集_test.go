213d2ebf46a8fc54dc201919092f638ba2aaebc6
kingson4wu <kingson4wu@gmail.com>
2023-04-02 15:30:19 +0800

    f

--- exercise/leetcode/hot100/78. 子集_test.go
+++ exercise/leetcode/hot100/78. 子集_test.go
@@ -68,3 +68,49 @@ func TestSubsets(t *testing.T) {
 	fmt.Println(subsets([]int{9, 0, 3, 5, 7}))
 	//为什么是[[] [7] [5] [5 7] [3] [3 7] [3 5] [3 5 7] [0] [0 7] [0 5] [0 5 7] [0 3] [0 3 7] [0 3 5] [0 3 5 7] [9] [9 7] [9 5] [9 5 7] [9 3] [9 3 7] [9 3 5] [9 3 5 7] [9 0] [9 0 7] [9 0 5] [9 0 5 7] [9 0 3] [9 0 3 7] [9 0 3 7] [9 0 3 7 7]]
 }
+
+/**
+方法一：迭代法实现子集枚举
+func subsets(nums []int) (ans [][]int) {
+    n := len(nums)
+    for mask := 0; mask < 1<<n; mask++ {
+        set := []int{}
+        for i, v := range nums {
+            if mask>>i&1 > 0 {
+                set = append(set, v)
+            }
+        }
+        ans = append(ans, append([]int(nil), set...))
+    }
+    return
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/subsets/solutions/420294/zi-ji-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+方法二：递归法实现子集枚举
+
+func subsets(nums []int) (ans [][]int) {
+    set := []int{}
+    var dfs func(int)
+    dfs = func(cur int) {
+        if cur == len(nums) {
+            ans = append(ans, append([]int(nil), set...))
+            return
+        }
+        set = append(set, nums[cur])
+        dfs(cur + 1)
+        set = set[:len(set)-1]
+        dfs(cur + 1)
+    }
+    dfs(0)
+    return
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/subsets/solutions/420294/zi-ji-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
