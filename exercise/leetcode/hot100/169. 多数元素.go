98388ad41f9c08ac83f904a42d557ae0c571810e
kingson4wu <kingson4wu@gmail.com>
2023-04-02 21:51:25 +0800

    169. 多数元素

--- exercise/leetcode/hot100/169. 多数元素.go
+++ exercise/leetcode/hot100/169. 多数元素.go
@@ -1,5 +1,14 @@
 package hot100
 
+/**
+
+看了题解再实现的
+
+对数字进行计数，相同加1，不同减一
+根据题目的前提条件，多数元素大于【n/2】，那么最后存在的那个数一定是多数元素
+
+*/
+
 /*
 *
 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
@@ -19,9 +28,21 @@ package hot100
 */
 func majorityElement(nums []int) int {
 
-	for i := 0; i < len(nums); i++ {
+	count := 0
+	current := 0
+	for _, num := range nums {
 
+		if count == 0 {
+			current = num
+			count++
+			continue
+		}
+
+		if current == num {
+			count++
+			continue
+		}
+		count--
 	}
-
-	return 0
+	return current
 }
