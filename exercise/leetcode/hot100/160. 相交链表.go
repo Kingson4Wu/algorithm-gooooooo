c3b6bded6d210ff328aa3dbd8a995c32ee03cbc3
kingson4wu <kingson4wu@gmail.com>
2023-04-02 23:15:32 +0800

    160. 相交链表

---
+++ exercise/leetcode/hot100/160. 相交链表.go
@@ -0,0 +1,71 @@
+package hot100
+
+/**
+简单级别的题目，没会，看了题解，有了启发，但是题解太复杂，我用自己的解法来
+
+1、双指针分别指向A和B，同时移动，先到达的，等待未到达的，计算等待的距离
+2、双指针重新开始，慢的先走之前计算的距离，然后同时走，当节点相同时，则为相交的结点
+
+好像不用那么复杂， 第一步优化成分别算出各自的长度即可
+
+超出时间限制 !!!
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	pA, pB := headA, headB
+	lenA, lenB := 0, 0
+
+	for {
+		lenA++
+		if pA.Next == nil {
+			break
+		}
+	}
+
+	for {
+		lenB++
+		if pB.Next == nil {
+			break
+		}
+	}
+
+	if pA != pB {
+		return nil
+	}
+
+	pA, pB = headA, headB
+	if lenA > lenB {
+		preGo := lenA - lenB
+		for i := 0; i < preGo; i++ {
+			pA = pA.Next
+		}
+	} else {
+		preGo := lenB - lenA
+		for i := 0; i < preGo; i++ {
+			pB = pB.Next
+		}
+	}
+
+	for {
+		if pA == pB {
+			return pA
+		}
+		pA = pA.Next
+		pB = pB.Next
+	}
+
+	return nil
+}
