13a43848a23c83ddddc4260f13a09d47d03ea898
kingson4wu <kingson4wu@gmail.com>
2023-04-03 23:10:36 +0800

    102. 二叉树的层序遍历

--- exercise/leetcode/hot100/102. 二叉树的层序遍历.go
+++ exercise/leetcode/hot100/102. 二叉树的层序遍历.go
@@ -5,6 +5,17 @@ nowcoder/BM26 求二叉树的层序遍历.go
 
 以前使用栈做的，这次尝试使用递归
 
+看了递归，实现也不简单，也要辅助hash
+但比起要自己维护栈还是简单点
+
+时间
+4 ms
+击败
+34.30%
+内存
+3 MB
+击败
+5.75%
 
 */
 
@@ -16,7 +27,24 @@ nowcoder/BM26 求二叉树的层序遍历.go
  *     Right *TreeNode
  * }
  */
-func levelOrder(root *TreeNode) (ans [][]int) {
+func levelOrder(root *TreeNode) [][]int {
+	highMap := make(map[int][]int)
+	var highF func(*TreeNode, int)
+	highF = func(root *TreeNode, high int) {
+		if root == nil {
+			return
+		}
+		highMap[high] = append(highMap[high], root.Val)
+		highF(root.Left, high+1)
+		highF(root.Right, high+1)
+	}
 
-	return
+	highF(root, 0)
+
+	var result [][]int
+	for i := 0; i < len(highMap); i++ {
+		result = append(result, highMap[i])
+	}
+
+	return result
 }
