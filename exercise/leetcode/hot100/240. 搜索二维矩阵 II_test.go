c59aec10dc7592a0fa176a47ac0bd7c5883c4801
kingson4wu <kingson4wu@gmail.com>
2023-04-03 18:03:58 +0800

    240. 搜索二维矩阵 II

--- exercise/leetcode/hot100/240. 搜索二维矩阵 II_test.go
+++ exercise/leetcode/hot100/240. 搜索二维矩阵 II_test.go
@@ -12,6 +12,8 @@ import (
 
 虽然是自己写的，但感觉写得太复杂太乱了
 
+使用官方答案的第三种方法，Z型，从右上角开始比对，移动即可
+
 时间
 28 ms
 击败
@@ -21,7 +23,48 @@ import (
 击败
 32.2%
 
+官方答案：
 
+方法二：二分查找
+思路与算法
+
+由于矩阵 matrix 中每一行的元素都是升序排列的，因此我们可以对每一行都使用一次二分查找，判断 target 是否在该行中，从而判断 target 是否出现。
+
+方法三：Z 字形查找
+思路与算法
+
+垃圾说明，直接看代码就好了
+
+我们可以从矩阵 matrix 的右上角 (0,n−1) 进行搜索。在每一步的搜索过程中，如果我们位于位置 (x,y)，那么我们希望在以 matrix 的左下角为左下角、以 (x,y) 为右上角的矩阵中进行搜索，即行的范围为 [x,m−1]，列的范围为 [0,y]：
+
+如果 matrix[x,y]=target，说明搜索完成；
+
+如果 matrix[x,y]>target，由于每一列的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 y 列的元素都是严格大于 target 的，因此我们可以将它们全部忽略，即将 y 减少 1；
+
+如果 matrix[x,y]<target，由于每一行的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 x 行的元素都是严格小于 target 的，因此我们可以将它们全部忽略，即将 x 增加 1。
+
+在搜索的过程中，如果我们超出了矩阵的边界，那么说明矩阵中不存在 target。
+
+func searchMatrix(matrix [][]int, target int) bool {
+    m, n := len(matrix), len(matrix[0])
+    x, y := 0, n-1
+    for x < m && y >= 0 {
+        if matrix[x][y] == target {
+            return true
+        }
+        if matrix[x][y] > target {
+            y--
+        } else {
+            x++
+        }
+    }
+    return false
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/search-a-2d-matrix-ii/solutions/1062538/sou-suo-er-wei-ju-zhen-ii-by-leetcode-so-9hcx/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
 
 /**
