0e42f6e75e65db6fd7f5e9869763f7a7b91c966e
kingson4wu <kingson4wu@gmail.com>
2023-04-07 14:35:23 +0800

    344. 反转字符串

---
+++ exercise/leetcode/tencent/344. 反转字符串.go
@@ -0,0 +1,28 @@
+package tencent
+
+/*
+*
+时间
+28 ms
+击败
+82.25%
+内存
+6.3 MB
+击败
+53.18%
+*/
+func reverseString(s []byte) {
+
+	if len(s) <= 1 {
+		return
+	}
+
+	start, end := 0, len(s)-1
+
+	for start < end {
+		s[start], s[end] = s[end], s[start]
+		start++
+		end--
+	}
+
+}
