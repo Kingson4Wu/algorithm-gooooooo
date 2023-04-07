640767b5323f98a3ae3fd4715672795a52fce321
kingson4wu <kingson4wu@gmail.com>
2023-04-07 10:49:45 +0800

    88. 合并两个有序数组

--- exercise/leetcode/tencent/26. 删除有序数组中的重复项_test.go
+++ exercise/leetcode/tencent/26. 删除有序数组中的重复项_test.go
@@ -20,6 +20,26 @@ import (
 39.21%
 */
 
+/**
+还有一种方法叫快慢指针
+
+int removeDuplicates(int[] nums) {
+    int n = nums.length;
+    if (n == 0) return 0;
+    int slow = 0, fast = 1;
+    while (fast < n) {
+        if (nums[fast] != nums[slow]) {
+            slow++;
+            // 维护 nums[0..slow] 无重复
+            nums[slow] = nums[fast];
+        }
+    fast++;
+    }
+    // ⻓度为索引 + 1
+    return slow + 1;
+}
+*/
+
 /*
 *
 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
---
+++ exercise/leetcode/tencent/88. 合并两个有序数组_test.go
@@ -0,0 +1,72 @@
+package tencent
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+*
+从后面开始遍历， 注意m==0的情况
+
+时间
+0 ms
+击败
+100%
+内存
+2.2 MB
+击败
+43.54%
+*/
+
+/*
+*
+示例 1：
+
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+解释：需要合并 [1,2,3] 和 [2,5,6] 。
+合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+示例 2：
+
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+解释：需要合并 [1] 和 [] 。
+合并结果是 [1] 。
+示例 3：
+
+输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+输出：[1]
+解释：需要合并的数组是 [] 和 [1] 。
+合并结果是 [1] 。
+注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+*/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	//j<0 说明数组1为空
+	j := m - 1
+	index := m + n - 1
+	for i := n - 1; i >= 0; i-- {
+		if j < 0 || nums2[i] > nums1[j] {
+			nums1[index] = nums2[i]
+		} else {
+			nums1[index] = nums1[j]
+			j--
+			i++
+		}
+		index--
+	}
+}
+
+func TestMerge(t *testing.T) {
+
+	arr1 := []int{1, 2, 3, 0, 0, 0}
+	merge(arr1, 3, []int{2, 5, 6}, 3)
+	fmt.Println(arr1)
+	arr2 := []int{1}
+	merge(arr2, 1, []int{}, 0)
+	fmt.Println(arr2)
+	arr3 := []int{0}
+	merge(arr3, 0, []int{1}, 1)
+	fmt.Println(arr3)
+}
