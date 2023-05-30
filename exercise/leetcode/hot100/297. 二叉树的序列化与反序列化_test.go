616e4af475293dfc88cc16f703985585f0af01c6
kingson4wu <kingson4wu@gmail.com>
2023-05-30 16:57:06 +0800

    297. 二叉树的序列化与反序列化

--- exercise/leetcode/hot100/297. 二叉树的序列化与反序列化_test.go
+++ exercise/leetcode/hot100/297. 二叉树的序列化与反序列化_test.go
@@ -1,6 +1,11 @@
 package hot100
 
-import "testing"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
 
 /**
  * Definition for a binary tree node.
@@ -33,6 +38,13 @@ import "testing"
 
 */
 
+/**
+按照之前做题的回忆，树可以通过先序和中序来构造
+
+1、将树序列化 先序和中序字符串，数字之间使用-分割，先序和中序之间使用=分割
+2、将字符串反序列化成树，中序的第一个即根节点，可以把先序分成两半，并算出左右子树的长度
+*/
+
 type Codec struct {
 }
 
@@ -44,13 +56,82 @@ func Constructor2() Codec {
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
 
-	return ""
+	var firstOrder []string
+	/** 先序 */
+	var fOrder func(root *TreeNode)
+	fOrder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		if root.Left != nil {
+			fOrder(root.Left)
+		}
+		firstOrder = append(firstOrder, strconv.Itoa(root.Val))
+		if root.Right != nil {
+			fOrder(root.Right)
+		}
+	}
+	fOrder(root)
+
+	var middleOrder []string
+	/** 中序 */
+	var mOrder func(root *TreeNode)
+	mOrder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		middleOrder = append(middleOrder, strconv.Itoa(root.Val))
+		if root.Left != nil {
+			mOrder(root.Left)
+		}
+		if root.Right != nil {
+			mOrder(root.Right)
+		}
+	}
+	mOrder(root)
+
+	return strings.Join(firstOrder, "-") + "=" + strings.Join(middleOrder, "-")
 }
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
 
-	return &TreeNode{}
+	var buildTree func(fOrder, mOrder []int) *TreeNode
+	buildTree = func(fOrder, mOrder []int) *TreeNode {
+		if len(fOrder) == 0 {
+			return nil
+		}
+		root := &TreeNode{
+			Val: mOrder[0],
+		}
+		leftLength := 0
+		for _, v := range fOrder {
+			if v == mOrder[0] {
+				break
+			}
+			leftLength++
+		}
+
+		root.Left = buildTree(fOrder[:leftLength], mOrder[1:1+leftLength])
+		root.Right = buildTree(fOrder[leftLength+1:], mOrder[1+leftLength:])
+		return root
+	}
+
+	arr := strings.Split(data, "=")
+	fOrder := strings.Split(arr[0], "-")
+	mOrder := strings.Split(arr[1], "-")
+	var fO []int
+	var mO []int
+	for _, v := range fOrder {
+		val, _ := strconv.Atoi(v)
+		fO = append(fO, val)
+	}
+	for _, v := range mOrder {
+		val, _ := strconv.Atoi(v)
+		mO = append(mO, val)
+	}
+
+	return buildTree(fO, mO)
 }
 
 /**
@@ -62,5 +143,23 @@ func (this *Codec) deserialize(data string) *TreeNode {
  */
 
 func TestSerialize(t *testing.T) {
+
+	ser := Constructor2()
+	deser := Constructor2()
+	root := &TreeNode{
+		Val: 4,
+		Left: &TreeNode{
+			Val: 5,
+		},
+		Right: &TreeNode{
+			Val:   9,
+			Left:  &TreeNode{Val: 3},
+			Right: &TreeNode{Val: 1, Right: &TreeNode{Val: 2}},
+		},
+	}
+	data := ser.serialize(root)
+	fmt.Println(data)
+	ans := deser.deserialize(data)
+	fmt.Println(ans)
 
 }
