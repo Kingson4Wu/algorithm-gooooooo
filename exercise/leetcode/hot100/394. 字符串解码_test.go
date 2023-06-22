cd53f4d1e6d9117601a4fa0309fda6ea010b77ec
kingson4wu <kingson4wu@gmail.com>
2023-06-22 16:10:49 +0800

    394. 字符串解码

--- exercise/leetcode/hot100/394. 字符串解码_test.go
+++ exercise/leetcode/hot100/394. 字符串解码_test.go
@@ -22,6 +22,8 @@ import "testing"
 输出："accaccacc"
 示例 3：
 
+
+
 输入：s = "2[abc]3[cd]ef"
 输出："abcabccdcdcdef"
 示例 4：
@@ -42,20 +44,147 @@ s 中所有整数的取值范围为 [1, 300]
 1、使用栈的思想遍历时，存储左括号的下标
 */
 
+/**
+方法一：栈操作
+方法二：递归
+
+一看就会，写起来就废系列
+
+方法一：栈操作
+
+func decodeString(s string) string {
+    stk := []string{}
+    ptr := 0
+    for ptr < len(s) {
+        cur := s[ptr]
+        if cur >= '0' && cur <= '9' {
+            digits := getDigits(s, &ptr)
+            stk = append(stk, digits)
+        } else if (cur >= 'a' && cur <= 'z' || cur >= 'A' && cur <= 'Z') || cur == '[' {
+            stk = append(stk, string(cur))
+            ptr++
+        } else {
+            ptr++
+            sub := []string{}
+            for stk[len(stk)-1] != "[" {
+                sub = append(sub, stk[len(stk)-1])
+                stk = stk[:len(stk)-1]
+            }
+            for i := 0; i < len(sub)/2; i++ {
+                sub[i], sub[len(sub)-i-1] = sub[len(sub)-i-1], sub[i]
+            }
+            stk = stk[:len(stk)-1]
+            repTime, _ := strconv.Atoi(stk[len(stk)-1])
+            stk = stk[:len(stk)-1]
+            t := strings.Repeat(getString(sub), repTime)
+            stk = append(stk, t)
+        }
+    }
+    return getString(stk)
+}
+
+func getDigits(s string, ptr *int) string {
+    ret := ""
+    for ; s[*ptr] >= '0' && s[*ptr] <= '9'; *ptr++ {
+        ret += string(s[*ptr])
+    }
+    return ret
+}
+
+func getString(v []string) string {
+    ret := ""
+    for _, s := range v {
+        ret += s
+    }
+    return ret
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/decode-string/solutions/264391/zi-fu-chuan-jie-ma-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+方法二：递归
+
+var (
+    src string
+    ptr int
+)
+
+func decodeString(s string) string {
+    src = s
+    ptr = 0
+    return getString()
+}
+
+func getString() string {
+    if ptr == len(src) || src[ptr] == ']' {
+        return ""
+    }
+    cur := src[ptr]
+    repTime := 1
+    ret := ""
+    if cur >= '0' && cur <= '9' {
+        repTime = getDigits()
+        ptr++
+        str := getString()
+        ptr++
+        ret = strings.Repeat(str, repTime)
+    } else if cur >= 'a' && cur <= 'z' || cur >= 'A' && cur <= 'Z' {
+        ret = string(cur)
+        ptr++
+    }
+    return ret + getString()
+}
+
+func getDigits() int {
+    ret := 0
+    for ; src[ptr] >= '0' && src[ptr] <= '9'; ptr++ {
+        ret = ret * 10 + int(src[ptr] - '0')
+    }
+    return ret
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/decode-string/solutions/264391/zi-fu-chuan-jie-ma-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+*/
+
 func decodeString(s string) string {
 
-	/*var leftIndex []int
+	var leftIndex []int
+	var rightIndex []int
+
 	var leftCount []int
 
 	var result []rune
 
-	for i, ch := range s {
+	index1, index2 := 0, 0
+	count := 0
+
+	for _, ch := range s {
+
 		if ch == '[' {
-			leftIndex = append(leftIndex, i)
+			leftCount = append(leftCount, count)
+			leftIndex = append(leftIndex, index1)
+			rightIndex = append(rightIndex, index2)
 			continue
 		}
+		if ch == ']' {
+			/*c := leftCount[len(leftCount)-1]
+			leftCount = leftCount[:len(leftCount)-1]
+			l := leftIndex[len(leftIndex)-1]
+			leftIndex = leftIndex[:len(leftIndex)-1]
+			r := rightIndex[len(rightIndex)-1]
+			rightIndex = rightIndex[:len(rightIndex)-1]*/
+
+			//result = s[l:r] + result
+
+		}
 		result = append(result, ch)
-	}*/
+	}
 
 	return ""
 }
