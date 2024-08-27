f05ca1676e994628d94d2123922d02acc6806a36
kingson4wu <kingson4wu@gmail.com>
2024-08-27 14:14:53 +0800

    27.移除元素- 官方题解

--- exercise/leetcode/2024/27.移除元素_test.go
+++ exercise/leetcode/2024/27.移除元素_test.go
@@ -85,3 +85,26 @@ func TestRemoveElement(t *testing.T) {
 	fmt.Println(removeElement([]int{1, 2, 2, 4}, 2))
 	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
 }
+
+/**
+
+题解的双指针
+
+func removeElement(nums []int, val int) int {
+    left, right := 0, len(nums)
+    for left < right {
+        if nums[left] == val {
+            nums[left] = nums[right-1]
+            right--
+        } else {
+            left++
+        }
+    }
+    return left
+}
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/remove-element/solutions/730203/yi-chu-yuan-su-by-leetcode-solution-svxi/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
