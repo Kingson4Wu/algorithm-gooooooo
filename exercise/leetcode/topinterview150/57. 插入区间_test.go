9919d566a1de33ea6f786106eaa6a4f3d6abe856
kingson4wu <kingson4wu@gmail.com>
2025-06-26 18:41:30 +0800

    57. 插入区间

---
+++ exercise/leetcode/topinterview150/57. 插入区间_test.go
@@ -0,0 +1,87 @@
+package topinterview150
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+自己做的，搞清楚区间关系即可
+
+执行用时分布
+0
+ms
+击败
+100.00%
+复杂度分析
+消耗内存分布
+5.82
+MB
+击败
+73.96%
+
+*/
+
+/*
+*
+给你一个 无重叠的 ，按照区间起始端点排序的区间列表 intervals，其中 intervals[i] = [starti, endi] 表示第 i 个区间的开始和结束，并且 intervals 按照 starti 升序排列。同样给定一个区间 newInterval = [start, end] 表示另一个区间的开始和结束。
+
+在 intervals 中插入区间 newInterval，使得 intervals 依然按照 starti 升序排列，且区间之间不重叠（如果有必要的话，可以合并区间）。
+
+返回插入之后的 intervals。
+
+注意 你不需要原地修改 intervals。你可以创建一个新数组然后返回它。
+
+示例 1：
+
+输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
+输出：[[1,5],[6,9]]
+示例 2：
+
+输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+输出：[[1,2],[3,10],[12,16]]
+解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+*/
+func insert(intervals [][]int, newInterval []int) [][]int {
+
+	var results [][]int
+	finish := false
+
+	for i, interval := range intervals {
+
+		if newInterval[1] < interval[0] {
+			finish = true
+			results = append(results, newInterval)
+			results = append(results, intervals[i:]...)
+			break
+		} else if newInterval[0] > interval[1] {
+			results = append(results, interval)
+		} else {
+			newInterval[0] = min(newInterval[0], interval[0])
+			newInterval[1] = max(newInterval[1], interval[1])
+		}
+	}
+	if !finish {
+		results = append(results, newInterval)
+	}
+
+	return results
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func TestInsert(t *testing.T) {
+	fmt.Println(insert([][]int{{1, 3}, {6, 9}}, []int{2, 5}))
+	fmt.Println(insert([][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8}))
+}
