cf458e7c1575f25591a925190148a51e04bd6ca3
kingson4wu <kingson4wu@gmail.com>
2025-06-27 22:19:12 +0800

    106. 从中序与后序遍历序列构造二叉树

--- exercise/leetcode/topinterview150/106. 从中序与后序遍历序列构造二叉树_test.go
+++ exercise/leetcode/topinterview150/106. 从中序与后序遍历序列构造二叉树_test.go
@@ -14,8 +14,42 @@ package topinterview150
  *     Right *TreeNode
  * }
  */
+
+/*
+*
+通过自己画图计算可知，套路和105. 从前序与中序遍历序列构造二叉树 一样
+root的在前序的第一个，在后序的最后一个
+*/
 func buildTree(inorder []int, postorder []int) *TreeNode {
 
-	return nil
+	root := &TreeNode{Val: postorder[len(postorder)-1]}
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == postorder[len(postorder)-1] {
 
+			left := inorder[0:i]
+			if len(left) > 0 {
+				root.Left = buildTree(left, postorder[0:len(left)])
+			}
+			right := inorder[i+1:]
+			if len(right) > 0 {
+				root.Right = buildTree(right, postorder[len(left):len(left)+len(right)])
+			}
+			break
+		}
+	}
+	return root
 }
+
+/**
+执行用时分布
+0
+ms
+击败
+100.00%
+复杂度分析
+消耗内存分布
+5.53
+MB
+击败
+96.40%
+*/
