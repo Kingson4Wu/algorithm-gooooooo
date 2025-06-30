3253d858bb0930047f6091c1bb7f26b27995cb0b
kingson4wu <kingson4wu@gmail.com>
2025-06-30 23:58:11 +0800

    206. 反转链表

--- exercise/leetcode/hot100/206. 反转链表.go
+++ exercise/leetcode/hot100/206. 反转链表.go
@@ -20,6 +20,29 @@ nowcoder/BM1 反转链表.go
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-
-	return nil
+	var pre, next *ListNode
+	for head != nil {
+		next = head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
 }
+
+/**
+这次做怎么这么顺利，很快做完
+
+执行用时分布
+0
+ms
+击败
+100.00%
+复杂度分析
+消耗内存分布
+4.25
+MB
+击败
+72.03%
+
+*/
