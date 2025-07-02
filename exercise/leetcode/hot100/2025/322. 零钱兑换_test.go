ca810aaf4bb9bbebc5b3c408a244d1ddf9a66d07
kingson4wu <kingson4wu@gmail.com>
2025-07-02 12:29:46 +0800

    /322. 零钱兑换

---
+++ exercise/leetcode/hot100/2025/322. 零钱兑换_test.go
@@ -0,0 +1,63 @@
+package _025
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+*
+动态规划，自己想的
+coin先放map
+dp := make([]int, amount+1)
+dp[i] 代表 金额 i的最小组合
+dp[i] = min(dp[k] + dp[i-k]); 1 <= k <= i/2
+结果是 dp[amount]
+
+超出时间限制
+188 / 189 个通过的测试用例
+最后执行的输入
+添加到测试用例
+coins =
+[1]
+amount =
+10000
+*/
+func coinChange(coins []int, amount int) int {
+
+	m := make(map[int]bool)
+	for i := 0; i < len(coins); i++ {
+		m[coins[i]] = true
+	}
+	dp := make([]int, amount+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = -1
+		if m[i] {
+			dp[i] = 1
+			continue
+		}
+		for k := 1; k <= i/2; k++ {
+			if dp[k] == -1 || dp[i-k] == -1 {
+				continue
+			}
+			if dp[i] == -1 {
+				dp[i] = dp[k] + dp[i-k]
+			} else {
+				dp[i] = min(dp[i], dp[k]+dp[i-k])
+			}
+		}
+	}
+	return dp[amount]
+}
+
+func TestCoinChange(t *testing.T) {
+	fmt.Println(coinChange([]int{1, 2, 5}, 11))
+	fmt.Println(coinChange([]int{2}, 3))
+	fmt.Println(coinChange([]int{1}, 0))
+}
+
+/**
+3
+-1
+0
+*/
