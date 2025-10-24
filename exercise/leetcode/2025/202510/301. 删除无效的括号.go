a8175ba5bb84a20343e8ef73db516f48b8126b72
kingson4wu <kingson4wu@gmail.com>
2025-10-24 23:16:58 +0800

    fix 301. 删除无效的括号

--- exercise/leetcode/2025/202510/301. 删除无效的括号.go
+++ exercise/leetcode/2025/202510/301. 删除无效的括号.go
@@ -1,5 +1,19 @@
 package _02510
 
+/*
+*
+执行用时分布
+0
+ms
+击败
+100.00%
+复杂度分析
+消耗内存分布
+4.22
+MB
+击败
+67.07%
+*/
 func removeInvalidParentheses(s string) []string {
 
 	isValid := func(s string) bool {
@@ -7,14 +21,16 @@ func removeInvalidParentheses(s string) []string {
 		for i := 0; i < len(s); i++ {
 			if s[i] == '(' {
 				count++
-			} else {
+			} else if s[i] == ')' {
 				if count == 0 {
 					return false
 				}
 				count--
 			}
 		}
-		return true
+		//漏了这句！！！！
+		//return true
+		return count == 0
 	}
 
 	findRemoveCount := func(s string) (int, int) {
@@ -22,7 +38,7 @@ func removeInvalidParentheses(s string) []string {
 		for i := 0; i < len(s); i++ {
 			if s[i] == '(' {
 				lRemove++
-			} else {
+			} else if s[i] == ')' {
 				if lRemove > 0 {
 					lRemove--
 				} else {
@@ -36,8 +52,13 @@ func removeInvalidParentheses(s string) []string {
 	var ans []string
 	var dfs func(s string, start int, lRemove, rRemove int)
 	dfs = func(s string, start int, lRemove, rRemove int) {
-		if start == len(s) && lRemove == 0 && rRemove == 0 && isValid(s) {
-			ans = append(ans, s)
+		//当字符串还没遍历完但括号已删光的情况会被跳过；
+		//isValid 的检查应该在所有删完时进行；
+		//不应该要求 start == len(s) 才算完，因为删除字符时 len(s) 在变。
+		if lRemove == 0 && rRemove == 0 {
+			if isValid(s) {
+				ans = append(ans, s)
+			}
 			return
 		}
 		for i := start; i < len(s); i++ {
@@ -47,10 +68,13 @@ func removeInvalidParentheses(s string) []string {
 			if lRemove+rRemove > len(s)-start {
 				break
 			}
-			if s[i] == '(' {
-				dfs(s[:start]+s[start+1:], start+1, lRemove-1, rRemove)
-			} else if s[i] == ')' {
-				dfs(s[:start]+s[start+1:], start+1, lRemove, rRemove-1)
+			if lRemove > 0 && s[i] == '(' {
+				//dfs(s[:start]+s[start+1:], start+1, lRemove-1, rRemove)
+				dfs(s[:i]+s[i+1:], i, lRemove-1, rRemove)
+				//元素已经被删了，应该用i而不是i+1
+			} else if rRemove > 0 && s[i] == ')' {
+				//dfs(s[:start]+s[start+1:], start+1, lRemove, rRemove-1)
+				dfs(s[:i]+s[i+1:], i, lRemove, rRemove-1)
 			}
 		}
 	}
