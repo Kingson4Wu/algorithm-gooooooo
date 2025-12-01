62cd5d207b6bb160fdba9f4cacb37e0e0ff9415d
kingson4wu <kingson4wu@gmail.com>
2025-12-01 18:43:40 +0800

    LCR 055. 二叉搜索树迭代器

---
+++ exercise/leetcode/LCR/LCR 055. 二叉搜索树迭代器.go
@@ -0,0 +1,105 @@
+package LCR
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+*
+实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+
+BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+int next()将指针向右移动，然后返回指针处的数字。
+注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+
+可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+
+示例：
+
+输入：
+inputs = ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+inputs = [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+输出：
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+解释：
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // 返回 3
+bSTIterator.next();    // 返回 7
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 9
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 15
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 20
+bSTIterator.hasNext(); // 返回 False
+
+提示：
+
+树中节点的数目在范围 [1, 105] 内
+0 <= Node.val <= 106
+最多调用 105 次 hasNext 和 next 操作
+
+进阶：
+
+你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h 是树的高度。
+*/
+
+/*
+*
+执行用时分布
+23
+ms
+击败
+39.13%
+复杂度分析
+消耗内存分布
+11.61
+MB
+击败
+60.87%
+复杂度分析
+
+中序遍历的栈形式
+*/
+type BSTIterator struct {
+	root  *TreeNode
+	stack []*TreeNode
+}
+
+func Constructor2(root *TreeNode) BSTIterator {
+
+	return BSTIterator{
+		root: root,
+	}
+}
+
+func (this *BSTIterator) Next() int {
+	for this.root != nil {
+		this.stack = append(this.stack, this.root)
+		this.root = this.root.Left
+	}
+	this.root = this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	defer func() {
+		this.root = this.root.Right
+	}()
+	return this.root.Val
+}
+
+func (this *BSTIterator) HasNext() bool {
+	return this.root != nil || len(this.stack) > 0
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
---
+++ exercise/leetcode/LCR/LCR 165. 解密数字_test.go
@@ -0,0 +1,31 @@
+package LCR
+
+/*
+*
+现有一串神秘的密文 ciphertext，经调查，密文的特点和规则如下：
+
+密文由非负整数组成
+数字 0-25 分别对应字母 a-z
+请根据上述规则将密文 ciphertext 解密为字母，并返回共有多少种解密结果。
+
+示例 1：
+
+输入：ciphertext = 216612
+输出：6
+解释：216612 解密后有 6 种不同的形式，分别是 "cbggbc"，"vggbc"，"vggm"，"cbggm"，"cqgbc" 和 "cqgm"
+
+提示：
+
+0 <= ciphertext < 231
+*/
+/**
+一开始想用回溯解题，不小心瞄到题解可以用动态规划
+然而还是不会写。。。
+
+看了题解，说个打家劫舍非常相似
+
+*/
+func crackNumber(ciphertext int) int {
+
+	return 0
+}
